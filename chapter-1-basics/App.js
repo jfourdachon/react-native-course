f75cca69c0ac57073b64e1d371de29791686ad97
JdevDyma <j.stepbytech@gmail.com>
2024-01-18 09:03:44 +0900

    back to images

--- chapter-1-basics/App.js
+++ chapter-1-basics/App.js
@@ -1,9 +1,14 @@
-import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { StyleSheet, SafeAreaView } from "react-native";
-import CustomModal from "./components/CustomModal";
-import ItemsList from "./components/ItemsList";
-import OpenModalButton from "./components/OpenModalBtutton";
+import {
+  StyleSheet,
+  SafeAreaView,
+  TouchableOpacity,
+  View,
+  Text,
+  TextInput,
+  FlatList,
+  Modal,
+} from "react-native";
 
 export default function App() {
   const [inputValue, setInputValue] = useState("");
@@ -24,15 +29,53 @@ export default function App() {
   };
   return (
     <SafeAreaView style={styles.container}>
-      <OpenModalButton onPress={onOpenModal} />
-      <ItemsList data={inputResult} />
-      <CustomModal
-        isModalVisible={isModalVisible}
-        inputValue={inputValue}
-        setInputValue={setInputValue}
-        onCreateItem={onCreateItem}
-        onCloseModal={onCloseModal}
-      />
+      <View style={styles.btnContainer}>
+        <TouchableOpacity
+          activeOpacity={0.9}
+          style={styles.btn}
+          onPress={onOpenModal}
+        >
+          <Text style={styles.btnText}>Ajouter un élément</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.resultContainer}>
+        <FlatList
+          showsVerticalScrollIndicator={false}
+          data={inputResult}
+          renderItem={({ item }) => {
+            return (
+              <View style={styles.itemContainer}>
+                <Text style={styles.item}>{item}</Text>
+              </View>
+            );
+          }}
+        />
+      </View>
+      <Modal visible={isModalVisible} animationType="slide">
+        <View style={styles.modalView}>
+          <View style={styles.formContainer}>
+            <TextInput
+              value={inputValue}
+              onChangeText={setInputValue}
+              style={styles.input}
+            />
+            <View style={styles.modalBtnContainer}>
+              <TouchableOpacity
+                style={[styles.createBtn, styles.modalBtns]}
+                onPress={onCreateItem}
+              >
+                <Text style={styles.modalBtnText}>Créer</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={[styles.closeBtn, styles.modalBtns]}
+                onPress={onCloseModal}
+              >
+                <Text style={styles.modalBtnText}>Fermer</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </Modal>
     </SafeAreaView>
   );
 }
@@ -41,5 +84,85 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
+  },
+  btnContainer: {
+    flexDirection: "row",
+    width: "100%",
+    paddingHorizontal: 8,
+  },
+  btn: {
+    width: "100%",
+    height: 50,
+    borderRadius: 8,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "violet",
+    marginTop: 12,
+  },
+  btnText: {
+    color: "white",
+    fontSize: 20,
+  },
+  resultContainer: {
+    flex: 1,
+    width: "100%",
+    padding: 8,
+  },
+  itemContainer: {
+    width: "100%",
+    height: 38,
+    marginVertical: 12,
+    borderRadius: 8,
+    backgroundColor: "rgba(0,0,0,0.75)",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  item: {
+    color: "white",
+    fontSize: 20,
+  },
+  modalView: {
+    flex: 1,
+    backgroundColor: "grey",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: 24,
+  },
+  formContainer: {
+    width: "100%",
+  },
+  input: {
+    width: "100%",
+    backgroundColor: "white",
+    borderWidth: 1,
+    borderColor: "grey",
+    borderRadius: 8,
+    height: 42,
+    fontSize: 20,
+    paddingHorizontal: 8,
+    paddingVertical: 4,
+  },
+  modalBtnContainer: {
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-between",
+  },
+  modalBtns: {
+    width: "40%",
+    height: 50,
+    borderRadius: 8,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 16,
+  },
+  createBtn: {
+    backgroundColor: "blue",
+  },
+  closeBtn: {
+    backgroundColor: "black",
+  },
+  modalBtnText: {
+    color: "white",
+    fontSize: 20,
   },
 });
